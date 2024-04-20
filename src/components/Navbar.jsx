d5501a584c0ba2bfe5fc88e5a987483c2cc4bea7
TuKK2 <luchogonzalez2004@gmail.com>
2024-04-20 12:58:58 -0300

    minor updates

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -16,10 +16,10 @@ function classNames(...classes) {
 
 export default function NavBar() {
   return (
-    <Disclosure as="nav" className='sticky top-0 w-full z-10'>
+    <Disclosure as="nav" className='sticky top-0 w-full z-10 font-roboto'>
       {({ open }) => (
         <>
-          <div className="mx-auto max-w-full px-2 sm:px-6 lg:px-8 border-b backdrop-blur-xl border-gray-500">
+          <div className="mx-auto max-w-full px-2 sm:px-6 lg:px-5 border-b backdrop-blur-xl border-gray-500">
             <div className="relative flex h-16 items-center justify-between">
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        roboto: ["Roboto", "sans-serif"],
+        poppins:["Poppins", "sans-serif"]
+      },
       keyframes: {
         FadeInLeft: {
           "0%": { opacity: "0", transform: "translateX(-100%)" },
